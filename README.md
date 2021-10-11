b3e3486e0d57ce8562ca8bba6ab449ed602f02ee
epcr <epcrmail@gmail.com>
2021-10-11 15:48:57 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# deepracer-setup-for-wsl2
- This repository contains a linux shell file for easily installing deepracer-for-cloud on Windows Subsystem for Linux  2 (wsl2).
+# Details
+This repository contains a Linux shell file for easily installing deepracer-for-cloud on Windows Subsystem for Linux 2 (wsl2).
+
+### Disclaimer
+This repository is not meant to be used in a production environment.
+
+It has only been tested on Ubuntu for wsl2 using an Nvidia GPU and AMD CPU.
+
+### Prerequisites
+ - Nvidia drivers compatible with wsl2
+
+## Usage
+Execute this in your Ubuntu shell with sudo permissions:
+
+	git clone https://github.com/TheEpiCreator/deepracer-setup-for-wsl2 && sudo ./deepracer-setup-for-wsl2/setup.sh
---
+++ setup.sh
@@ -0,0 +1,23 @@
+sudo apt-get update
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-4-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+curl https://get.docker.com | sh
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo service docker stop
+sudo service docker start
+sudo apt-get install jq awscli python3-boto3 docker-compose
+cat /etc/docker/daemon.json | jq 'del(."default-runtime") + {"default-runtime": "nvidia"}' | sudo tee /etc/docker/daemon.json
+sudo usermod -a -G docker $(id -un)
+git clone https://github.com/aws-deepracer-community/deepracer-for-cloud.git
+cd deepracer-for-cloud
+sudo bin/init.sh -a gpu -c local
+sudo sed -i 's/^{/{\n\t"default-runtime": "nvidia",/' /etc/docker/daemon.json
