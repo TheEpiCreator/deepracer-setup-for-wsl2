16b34589c9a11ee538ffbec10ade9142d0e2f2be
epcr <epcrmail@gmail.com>
2021-10-14 17:36:03 -0500

    Fixed(?) prompts

--- setup.sh
+++ setup.sh
@@ -18,10 +18,10 @@ echo "${GREEN}You may have to enter some data during configuration.\nThis may ta
 
 # get GPU info
 read -p "Are you running this on WSL2 for win11? (Only answer 'no' if you know what you're doing) [Y/n]: " -r HASWSLREP
-HASWSL = [[ $HASWSLREP =~ ^[Yy]$ ]]
+HASWSL=[[ $HASWSLREP =~ ^[Yy]$ ]]
 
 read -p "Is your GPU a 30-series (3070, 3080ti, 3060 super, etc.)? [Y/n]: " -r HASNVREP
-HASNV = [[ $HASNVREP =~ ^[Yy]$ ]]
+HASNV=[[ $HASNVREP =~ ^[Yy]$ ]]
 
 
 # install appropriate nvidia toolkit(s)
